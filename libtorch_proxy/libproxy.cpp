50eedc643db7308d86d590740af829ec08627328
liuly <me@liuly.moe>
2025-01-20 17:43:26 +0800

    chore: format

--- libtorch_proxy/libproxy.cpp
+++ libtorch_proxy/libproxy.cpp
@@ -1,42 +1,45 @@
-#include <torch/torch.h>
 #include <iostream>
-#include <vector>
 #include <map>
+#include <torch/torch.h>
+#include <vector>
 
 std::vector<float> for_poc_internal() {
-  torch::Tensor tensor = torch::rand({2, 3});
-  std::vector<float> vec(tensor.data_ptr<float>(),
-                         tensor.data_ptr<float>() + tensor.numel());
-  return vec;
+    torch::Tensor tensor = torch::rand({2, 3});
+    std::vector<float> vec(tensor.data_ptr<float>(),
+                           tensor.data_ptr<float>() + tensor.numel());
+    return vec;
 }
 
 std::map<int, torch::Tensor> global_tensor_map;
 
-int at_tensor_of_data(void *vs, int64_t *dims, size_t ndims, size_t element_size_in_bytes, int type) {
-    torch::Tensor tensor = torch::zeros(torch::IntArrayRef(dims, ndims), torch::ScalarType(type));
+int at_tensor_of_data_internal(void *vs, int64_t *dims, size_t ndims,
+                               size_t element_size_in_bytes, int type) {
+    torch::Tensor tensor =
+        torch::zeros(torch::IntArrayRef(dims, ndims), torch::ScalarType(type));
     if ((int64_t)element_size_in_bytes != tensor.element_size())
-      throw std::invalid_argument("incoherent element sizes in bytes");
+        throw std::invalid_argument("incoherent element sizes in bytes");
     void *tensor_data = tensor.data_ptr();
     memcpy(tensor_data, vs, tensor.numel() * element_size_in_bytes);
     global_tensor_map.insert(
-        std::pair<int, torch::Tensor>(global_tensor_map.size(), tensor));
+        std::pair<int, torch::Tensor>(global_tensor_map.size(), std::move(tensor)));
     return global_tensor_map.size() - 1;
 }
 
-std::vector<unsigned char> get_tensor_raw(int global_id) {
-    torch::Tensor& tensor = global_tensor_map[global_id];
-    std::vector<unsigned char> vec(tensor.data_ptr<unsigned char>(),
-                                   tensor.data_ptr<unsigned char>() + tensor.numel());
+std::vector<unsigned char> get_tensor_raw_internal(int global_id) {
+    torch::Tensor &tensor = global_tensor_map[global_id];
+    void *tensor_data = tensor.data_ptr();
+    std::vector<unsigned char> vec(tensor.numel() * tensor.element_size());
+    memcpy(vec.data(), tensor_data, vec.size());
     return vec;
 }
 
-void drop_tensor(int global_id) { global_tensor_map.erase(global_id); }
+// void drop_tensor(int global_id) { global_tensor_map.erase(global_id); }
 
-int multiply_tensors(int global_id1, int global_id2) {
-    torch::Tensor& tensor1 = global_tensor_map[global_id1];
-    torch::Tensor& tensor2 = global_tensor_map[global_id2];
-    torch::Tensor result = tensor1 * tensor2;
-    global_tensor_map.insert(
-        std::pair<int, torch::Tensor>(global_tensor_map.size(), result));
-    return global_tensor_map.size() - 1;
-}
+// int multiply_tensors(int global_id1, int global_id2) {
+//     torch::Tensor& tensor1 = global_tensor_map[global_id1];
+//     torch::Tensor& tensor2 = global_tensor_map[global_id2];
+//     torch::Tensor result = tensor1 * tensor2;
+//     global_tensor_map.insert(
+//         std::pair<int, torch::Tensor>(global_tensor_map.size(), result));
+//     return global_tensor_map.size() - 1;
+// }
--- torch/native_stub.cpp
+++ torch/native_stub.cpp
@@ -1,17 +1,40 @@
 #include "moonbit.hpp"
 
+#include <cstring>
 #include <iostream>
 #include <vector>
-#include <cstring>
 
 std::vector<float> for_poc_internal();
 
-struct moonbit_bytes* for_poc() {
-  auto internal_result = for_poc_internal();
-  auto size = internal_result.size() * sizeof(float);
-  struct moonbit_bytes* bytes = moonbit_make_bytes(size, 0);
-  for (int i = 0; i < internal_result.size(); i++) {
-    memcpy(bytes->data + i * sizeof(float), &internal_result[i], sizeof(float));
-  }
-  return bytes;
+struct moonbit_bytes *for_poc() {
+    auto internal_result = for_poc_internal();
+    auto size = internal_result.size() * sizeof(float);
+    struct moonbit_bytes *bytes = moonbit_make_bytes(size, 0);
+    for (int i = 0; i < internal_result.size(); i++) {
+        memcpy(bytes->data + i * sizeof(float), &internal_result[i],
+               sizeof(float));
+    }
+    return bytes;
+}
+
+int at_tensor_of_data_internal(void *vs, int64_t *dims, size_t ndims,
+                               size_t element_size_in_bytes, int type);
+
+int at_tensor_of_data(struct moonbit_bytes *data_ptr,
+                      struct moonbit_bytes *dims, unsigned ndims,
+                      unsigned element_size_in_bytes, int type) {
+    return at_tensor_of_data_internal(data_ptr->data, (int64_t *)dims->data,
+                                      ndims, element_size_in_bytes, type);
+}
+
+std::vector<unsigned char> get_tensor_raw_internal(int global_id);
+
+struct moonbit_bytes *get_tensor_raw(int global_id) {
+    auto internal_result = get_tensor_raw_internal(global_id);
+    auto size = internal_result.size();
+    struct moonbit_bytes *bytes = moonbit_make_bytes(size, 0);
+    for (int i = 0; i < internal_result.size(); i++) {
+        bytes->data[i] = internal_result[i];
+    }
+    return bytes;
 }
