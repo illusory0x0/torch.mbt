2a9b2b18cc3443e137ce7606b97262104c6dae0a
liuly <me@liuly.moe>
2025-02-08 16:35:38 +0800

    chore: use asan in CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,8 +1,9 @@
+cp .github/workflows/moon.pkg.asan.json torch/moon.pkg.json
 rm -rf target
 cd libtorch_proxy
 mkdir -p build
 cd build
-cmake -DCMAKE_PREFIX_PATH=../libtorch ..
+cmake -DCMAKE_PREFIX_PATH=../libtorch -DUSE_ASAN=ON ..
 make -j
 cp libtchproxy.so ../../
 cd ../../
