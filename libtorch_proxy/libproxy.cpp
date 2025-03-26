6f39ae37bbc98cc8076cdf2ad6022796d7831de7
liuly <me@liuly.moe>
2025-03-26 09:22:06 +0800

    perf: placement new

--- libtorch_proxy/libproxy.cpp
+++ libtorch_proxy/libproxy.cpp
@@ -20,13 +20,13 @@ std::vector<char> get_the_bytes(std::string filename) {
 extern "C" {
 #include "moonbit.h"
 struct tensor_object_internal {
-    torch::Tensor *object;
+    torch::Tensor object;
 };
 
 void tensor_object_internal_delete(void *object) {
     struct tensor_object_internal *obj =
         (struct tensor_object_internal *)object;
-    delete obj->object;
+    obj->object.~Tensor();
 }
 
 struct tensor_object_internal *
@@ -35,7 +35,7 @@ tensor_object_internal_new(torch::Tensor object) {
         (struct tensor_object_internal *)moonbit_make_external_object(
             tensor_object_internal_delete,
             sizeof(struct tensor_object_internal));
-    obj->object = new torch::Tensor(object);
+    new (&obj->object) torch::Tensor(object);
     return obj;
 }
 
@@ -77,13 +77,13 @@ tensor_object_internal *at_tensor_of_data_internal(moonbit_bytes_t vs,
 tensor_object_internal *reshape_internal(tensor_object_internal *tensor,
                                          moonbit_bytes_t dims, size_t ndims) {
     auto result =
-        tensor->object->reshape(torch::IntArrayRef((int64_t *)dims, ndims));
+        tensor->object.reshape(torch::IntArrayRef((int64_t *)dims, ndims));
     moonbit_decref(dims);
     return tensor_object_internal_new(result);
 }
 
 moonbit_bytes_t get_tensor_raw_internal(tensor_object_internal *tensor) {
-    torch::Tensor contiguous_tensor = tensor->object->contiguous();
+    torch::Tensor contiguous_tensor = tensor->object.contiguous();
     void *tensor_data = contiguous_tensor.data_ptr();
     auto size = contiguous_tensor.numel() * contiguous_tensor.element_size();
     moonbit_bytes_t bytes = moonbit_make_bytes(size, 0);
@@ -92,12 +92,12 @@ moonbit_bytes_t get_tensor_raw_internal(tensor_object_internal *tensor) {
 }
 
 int get_tensor_length_internal(tensor_object_internal *tensor) {
-    return tensor->object->numel();
+    return tensor->object.numel();
 }
 
 moonbit_bytes_t get_tensor_shape_internal(tensor_object_internal *tensor) {
-    std::vector<unsigned> vec(tensor->object->sizes().begin(),
-                              tensor->object->sizes().end());
+    std::vector<unsigned> vec(tensor->object.sizes().begin(),
+                              tensor->object.sizes().end());
     moonbit_bytes_t bytes =
         moonbit_make_bytes(vec.size() * sizeof(unsigned), 0);
     memcpy(bytes, vec.data(), vec.size() * sizeof(unsigned));
@@ -105,79 +105,79 @@ moonbit_bytes_t get_tensor_shape_internal(tensor_object_internal *tensor) {
 }
 
 tensor_object_internal *abs_tensor_internal(tensor_object_internal *tensor) {
-    torch::Tensor result = torch::abs(*(tensor->object));
+    torch::Tensor result = torch::abs(tensor->object);
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *exp_tensor_internal(tensor_object_internal *tensor) {
-    torch::Tensor result = torch::exp(*(tensor->object));
+    torch::Tensor result = torch::exp(tensor->object);
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *log_tensor_internal(tensor_object_internal *tensor) {
-    torch::Tensor result = torch::log(*(tensor->object));
+    torch::Tensor result = torch::log(tensor->object);
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *add_tensors_internal(tensor_object_internal *tensor1,
                                              tensor_object_internal *tensor2) {
-    torch::Tensor result = *tensor1->object + *tensor2->object;
+    torch::Tensor result = tensor1->object + tensor2->object;
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *neg_tensor_internal(tensor_object_internal *tensor) {
-    torch::Tensor result = -*tensor->object;
+    torch::Tensor result = -tensor->object;
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *sub_tensors_internal(tensor_object_internal *tensor1,
                                              tensor_object_internal *tensor2) {
-    torch::Tensor result = *tensor1->object - *tensor2->object;
+    torch::Tensor result = tensor1->object - tensor2->object;
     return tensor_object_internal_new(result);
 }
 
 int equal_tensors_internal(tensor_object_internal *tensor1,
                            tensor_object_internal *tensor2) {
-    return torch::allclose(*tensor1->object, *tensor2->object);
+    return torch::allclose(tensor1->object, tensor2->object);
 }
 
 tensor_object_internal *mul_tensors_internal(tensor_object_internal *tensor1,
                                              tensor_object_internal *tensor2) {
-    torch::Tensor result = *tensor1->object * *tensor2->object;
+    torch::Tensor result = tensor1->object * tensor2->object;
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *
 matmul_tensors_internal(tensor_object_internal *tensor1,
                         tensor_object_internal *tensor2) {
-    torch::Tensor result = torch::matmul(*tensor1->object, *tensor2->object);
+    torch::Tensor result = torch::matmul(tensor1->object, tensor2->object);
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *
 transpose_tensor_internal(tensor_object_internal *tensor) {
-    torch::Tensor result = tensor->object->t();
+    torch::Tensor result = tensor->object.t();
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *argmin_tensor_internal(tensor_object_internal *tensor) {
-    torch::Tensor result = torch::argmin(*(tensor->object));
+    torch::Tensor result = torch::argmin(tensor->object);
     return tensor_object_internal_new(result);
 }
 
 tensor_object_internal *argmax_tensor_internal(tensor_object_internal *tensor) {
-    torch::Tensor result = torch::argmax(*(tensor->object));
+    torch::Tensor result = torch::argmax(tensor->object);
     return tensor_object_internal_new(result);
 }
 
 struct module_object_internal {
-    torch::jit::script::Module *object;
+    torch::jit::script::Module object;
 };
 
 void module_object_internal_delete(void *object) {
     struct module_object_internal *obj =
         (struct module_object_internal *)object;
-    delete obj->object;
+    obj->object.~Module();
 }
 
 struct module_object_internal *
@@ -186,7 +186,7 @@ module_object_internal_new(torch::jit::script::Module object) {
         (struct module_object_internal *)moonbit_make_external_object(
             module_object_internal_delete,
             sizeof(struct module_object_internal));
-    obj->object = new torch::jit::script::Module(object);
+    new (&obj->object) torch::jit::script::Module(object);
     return obj;
 }
 
@@ -208,7 +208,7 @@ module_object_internal *load_model_internal(moonbit_bytes_t path) {
 tensor_object_internal *forward_internal(module_object_internal *module,
                                          tensor_object_internal *tensor) {
     torch::Tensor result =
-        module->object->forward({*tensor->object}).toTensor();
+        module->object.forward({tensor->object}).toTensor();
     return tensor_object_internal_new(result);
 }
 }
